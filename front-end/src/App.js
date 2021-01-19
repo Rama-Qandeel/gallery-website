ce596ccfc6151ecaa0151f5399457473a47f05fb
Rama-Qandeel <rama.qndeel@gmail.com>
2021-01-19 23:30:40 +0200

    configr firebase with react

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,29 +1,16 @@
 import React, { Component } from 'react';
+import Upload from './components/Upload';
 import './App.css';
 
-// Class component
-export default class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-  render() {
-    return (
+const App =()=> {
+
+  return (
       <div>
-        <h1>HELLO WORLD</h1>
+       <Upload/>
       </div>
     );
   }
-}
 
-/* 
-// functional component
-const App = () => {
-  return (
-    <div>
-      <h1>HELLO WORLD</h1>
-    </div>
-  );
-};
-export default App;
-*/
+  export default App;
+
+
---
+++ front-end/src/components/Upload.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+
+const Upload = () => {
+    return (
+      <div>
+        <h1>HELLO WORLD</h1>
+      </div>
+    );
+  };
+
+  export default Upload;
