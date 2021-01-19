81a3606509188fdcd62f945763ac24c649e29f34
Rama-Qandeel <rama.qndeel@gmail.com>
2021-01-19 10:16:11 +0200

    starter code

---
+++ back-end/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const mainRouter = require('./routes/main-route');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(mainRouter);
+
+const PORT = 5000 || process.env.PORT;
+app.listen(PORT, () => console.log(`listening at http://localhost:${PORT}`));
