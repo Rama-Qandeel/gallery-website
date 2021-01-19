188d7d4be97b0a806a1db9dcec1fd2b8011472ed
Rama-Qandeel <rama.qndeel@gmail.com>
2021-01-19 23:52:30 +0200

    create component upload and handle the error

--- front-end/src/components/Upload.js
+++ front-end/src/components/Upload.js
@@ -1,10 +1,33 @@
-import React, { Component } from 'react';
+import React, { useState  } from 'react';
 
 
 const Upload = () => {
-    return (
+  const [file, setFile] = useState(null);
+  const [error, setError] = useState(null);
+
+  const types = ['image/png', 'image/jpeg'];
+
+  const handleChange = (e) => {
+    let selected = e.target.files[0];
+
+    if (selected && types.includes(selected.type)) {
+      setFile(selected);
+      setError('');
+    } else {
+      setFile(null);
+      setError('Please select an image file (png or jpg)');
+    }
+  }; 
+  
+  
+  
+  return (
       <div>
-        <h1>HELLO WORLD</h1>
+        <form>
+        <input type="file" onChange={handleChange} />
+      <div >{ error }</div>
+      <div>{file?(<p>{file.name}</p>):(null)}</div> 
+        </form>
       </div>
     );
   };
