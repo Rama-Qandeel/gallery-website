a60c3f67a63fe4fdda98e9bd2379ba10adca4d70
Rama-Qandeel <rama.qndeel@gmail.com>
2021-01-19 18:26:13 +0200

    return jwt

--- back-end/controllers/main-controller.js
+++ back-end/controllers/main-controller.js
@@ -1,35 +1,50 @@
-const firebase=require("firebase")
+const firebase = require("firebase");
+require("dotenv").config();
+const jwt = require("jsonwebtoken");
 
-const register=(req, res) => {
-    const email=req.body.email;
-    const password=req.body.password;
-     firebase
-     .auth()
-     .createUserWithEmailAndPassword(email, password)
-     .then(({ user }) => {
-      res.json("Account successfully created ")
-     }).catch((err)=>{
-       res.json(err)
-     })
-    }
-
-    
-    const login=(req, res) => {
-        const email=req.body.email;
-        const password=req.body.password;
-        firebase.auth().signInWithEmailAndPassword(email, password)
-    .then((user)=>{
-        res.json("found")
+const register = (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  firebase
+    .auth()
+    .createUserWithEmailAndPassword(email, password)
+    .then(({ user }) => {
+      res.json("Account successfully created ");
     })
-        .catch(function(error) {
-  // Handle Errors here.
-  var errorCode = error.code;
-  var errorMessage = error.message; 
-  if (errorCode === 'auth/wrong-password') {
-    res.json('Wrong password.');
-  } else {
-    res.json(errorMessage);
-  }
-});
-        }
- module.exports = {register,login};
+    .catch((err) => {
+      res.json(err);
+    });
+};
+
+const login = (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  firebase
+    .auth()
+    .signInWithEmailAndPassword(email, password)
+    .then((user) => {
+    //   console.log("user", user);
+      const payload = {
+        email,
+      };
+
+      const options = {
+        expiresIn: process.env.TOKEN_EXPIRATION,
+      };
+      
+      const token = jwt.sign(payload, process.env.SECRET, options);
+      res.json(token);
+    })
+    .catch(function (error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      if (errorCode === "auth/wrong-password") {
+        res.json("Wrong password.");
+      } else {
+        res.json(errorMessage);
+      }
+    });
+};
+module.exports = { register, login };
