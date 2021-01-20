ff7d91a022ee3cff8dc0f40d5393c722cf8853fa
Rama-Qandeel <rama.qndeel@gmail.com>
2021-01-20 19:06:16 +0200

    create function delete from database

--- front-end/src/components/ImageGrid.js
+++ front-end/src/components/ImageGrid.js
@@ -1,21 +1,37 @@
 import React from "react";
 import useFirestore from './useFirestore';
 import Popup from "./Popup"
+import { projectFirestore } from '../firebase/config';
 
 const ImageGrid = () => {
   const { docs } = useFirestore('images');
 // console.log('docs',docs);
+
+const deleteImage=(props)=>{
+// console.log(props.target.id);
+projectFirestore
+  .collection("images")
+  .doc(props.target.id)
+  .delete()
+  .then(() => {
+    console.log('done');
+  })
+  .catch((err) => {
+   console.log('errr',err);
+   
+  });
+}
+
 
 const render=docs.map(doc=>{
   // console.log('name',doc.name); 
   // if(doc.name==="rama"){
     return <div key={doc.id}>
       <img style={{height:"50px"}}src={doc.url} alt="uploaded pic"/>
-      <button>Delete image</button>
+      <button onClick={deleteImage} id={doc.id} >Delete image</button>
     </div>
   // }
 })
-
 
   return (
     <div >
