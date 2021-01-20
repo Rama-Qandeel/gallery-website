b7ff2185d275ace02cfe6f517b2a572639e82a6e
Rama-Qandeel <rama.qndeel@gmail.com>
2021-01-20 17:09:37 +0200

    show images in bage

--- front-end/src/components/ImageGrid.js
+++ front-end/src/components/ImageGrid.js
@@ -1,12 +1,33 @@
 import React from "react";
+import useFirestore from './useFirestore';
 
 const ImageGrid = () => {
+  const { docs } = useFirestore('images');
+// console.log('docs',docs);
+
+const render=docs.map(doc=>{
+  // console.log('name',doc.name); 
+  // if(doc.name==="rama"){
+    return <div  key={doc.id}      
+    layout
+      whileHover={{ opacity: 1 }}s
+    >
+      <img style={{height:"50px"}}src={doc.url} alt="uploaded pic"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ delay: 1 }}
+      />
+    </div>
+  // }
+  
+})
+
 
   return (
-  <div >
-Image
-  </div>
+    <div >
+      {render}
+    </div>
   )
-};
+}
 
 export default ImageGrid;
