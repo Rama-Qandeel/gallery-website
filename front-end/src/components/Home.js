b82d48774972157082a138d78acfe96edbebf38d
Rama-Qandeel <rama.qndeel@gmail.com>
2021-01-22 21:05:56 +0200

    create navbar component with style

--- front-end/src/components/Home.js
+++ front-end/src/components/Home.js
@@ -1,5 +1,6 @@
 import React from "react";
 import logo from "../images/Gallery-Logo.png";
+import Navbar from "./Navbar";
 
 const Home = () => {
   return (
@@ -8,12 +9,14 @@ const Home = () => {
         <div class="header__logo-box">
           <img src={logo} alt="Logo" class="header__logo" />
         </div>
+        <div className="header__nav-bar">
+        <Navbar/>
+        </div>  
         <div class="header__text-box">
                 <h1 class="heading-primary">
                     <span class="heading-primary--main">Photo Albums</span>
                     <span class="heading-primary--sub">Create Your Most Beautiful Album.</span>
                 </h1>
-
                 <a href="#" class="btn btn--gray btn--animated">Let's start</a>
             </div>
       </header>
---
+++ front-end/src/components/Navbar.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+const Navbar =()=> {
+   
+        return (
+          <nav>
+              <ul className="navigaton">
+                 <li className="navigaton__item"><a href="#">Home</a></li>
+                 <li className="navigaton__item"><a href="#">About</a></li>
+                 <li className="navigaton__item"><a href="#">Conatct</a></li>
+                 <li className="navigaton__item"><a href="#">Login</a></li>
+              </ul>
+          </nav>
+        )
+    }
+
+
+export default Navbar;
