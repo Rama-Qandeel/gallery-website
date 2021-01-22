22c2ec9d8e8950f5de6fdf84ee22779f6843f87b
Rama-Qandeel <rama.qndeel@gmail.com>
2021-01-22 23:56:32 +0200

    create grid-layout

--- front-end/src/components/About.js
+++ front-end/src/components/About.js
@@ -2,15 +2,25 @@ import React from "react";
 
 const About=()=>{
     return(
-        <div>
+      
         <section class="section-about">
                 <div class="u-center-text u-margin-bottom-big">
                     <h2 class="heading-secondary">
                     Experiencing memories
                     </h2>
-                </div>
+                    </div>
+                    <div class="row">
+                    <div class="col-1-of-2">
+                        <p class="paragraph">
+                        Create an album in minutes. 
+                        Relive your favorite photos anytime,
+                         as a photo book or on any device.
+                        </p>
+                    </div>
+               </div> 
+              
                 </section>
-        </div>
+       
     )
 }
 
